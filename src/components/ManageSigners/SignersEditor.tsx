c9aa1fbcf28e9cbb11ce2510a0ddb26dd1f76c20
Andy Wermke <andy.dev@next-step-software.com>
2018-11-23 18:05:52 +0100

    Allow editing signer weights. WIP

--- src/components/ManageSigners/SignersEditor.tsx
+++ src/components/ManageSigners/SignersEditor.tsx
@@ -5,6 +5,7 @@ import ListItem from "@material-ui/core/ListItem"
 import ListItemIcon from "@material-ui/core/ListItemIcon"
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction"
 import ListItemText from "@material-ui/core/ListItemText"
+import EditIcon from "@material-ui/icons/Edit"
 import PersonIcon from "@material-ui/icons/Person"
 import RemoveIcon from "@material-ui/icons/Close"
 import SpaciousList from "../List/SpaciousList"
@@ -16,16 +17,17 @@ type Signer = AccountRecord["signers"][0]
 interface SignersEditorProps {
   signers: Signer[]
   addSigner: (signer: Signer) => void
+  editSigner: (signer: Signer) => void
   removeSigner: (signer: Signer) => void
 }
 
 interface SignersEditorState {
-  editingNewSigner: boolean
-  newSignerErrors: {
+  editingSigner: "new" | "existing" | false
+  signerEditErrors: {
     publicKey?: Error
     weight?: Error
   }
-  newSignerValues: {
+  signerEditValues: {
     publicKey: string
     weight: string
   }
@@ -33,63 +35,74 @@ interface SignersEditorState {
 
 class SignersEditor extends React.Component<SignersEditorProps, SignersEditorState> {
   state: SignersEditorState = {
-    editingNewSigner: false,
-    newSignerErrors: {},
-    newSignerValues: {
+    editingSigner: false,
+    signerEditErrors: {},
+    signerEditValues: {
       publicKey: "",
       weight: "1"
     }
   }
 
   addAdditionalCosigner = () => {
-    this.setState({ editingNewSigner: true })
+    this.setState({ editingSigner: "new" })
   }
 
   cancelEditingCosigner = () => {
-    this.setState({ editingNewSigner: false })
+    this.setState({ editingSigner: false })
   }
 
   createCosigner = () => {
-    const { newSignerValues } = this.state
-    const errors: Partial<SignersEditorState["newSignerErrors"]> = {}
+    const { signerEditValues } = this.state
+    const errors: Partial<SignersEditorState["signerEditErrors"]> = {}
 
-    if (!newSignerValues.publicKey.match(/^G[A-Z0-9]{55}$/)) {
+    if (!signerEditValues.publicKey.match(/^G[A-Z0-9]{55}$/)) {
       errors.publicKey = new Error("Not a valid public key.")
     }
-    if (!newSignerValues.weight.match(/^[0-9]+$/)) {
+    if (!signerEditValues.weight.match(/^[0-9]+$/)) {
       errors.weight = new Error("Must be an integer.")
     }
 
     if (Object.keys(errors).length > 0) {
-      return this.setState({ newSignerErrors: errors })
+      return this.setState({ signerEditErrors: errors })
     }
 
     this.props.addSigner({
-      public_key: newSignerValues.publicKey,
-      weight: parseInt(newSignerValues.weight, 10)
+      public_key: signerEditValues.publicKey,
+      weight: parseInt(signerEditValues.weight, 10)
     })
 
     this.setState({
-      editingNewSigner: false,
-      newSignerErrors: {},
-      newSignerValues: {
+      editingSigner: false,
+      signerEditErrors: {},
+      signerEditValues: {
         publicKey: "",
         weight: "1"
       }
     })
   }
 
-  updateNewSignerValues = (values: Partial<SignersEditorState["newSignerValues"]>) => {
+  editSigner = (signer: Signer) => {
+    this.setState({
+      editingSigner: "existing",
+      signerEditErrors: {},
+      signerEditValues: {
+        publicKey: signer.public_key,
+        weight: String(signer.weight)
+      }
+    })
+  }
+
+  updateNewSignerValues = (values: Partial<SignersEditorState["signerEditValues"]>) => {
     this.setState(state => ({
-      newSignerValues: {
-        ...state.newSignerValues,
+      signerEditValues: {
+        ...state.signerEditValues,
         ...values
       }
     }))
   }
 
   render() {
-    const { editingNewSigner, newSignerErrors, newSignerValues } = this.state
+    const { editingSigner, signerEditErrors, signerEditValues } = this.state
     return (
       <SpaciousList fitHorizontal>
         {this.props.signers.map(signer => (
@@ -102,20 +115,22 @@ class SignersEditor extends React.Component<SignersEditorProps, SignersEditorSta
               secondary={`Weight: ${signer.weight}`}
             />
             <ListItemSecondaryAction>
-              {/* TODO: Edit weight */}
+              <IconButton aria-label="Edit" disabled={editingSigner !== false} onClick={() => this.editSigner(signer)}>
+                <EditIcon />
+              </IconButton>
               <IconButton aria-label="Remove" onClick={() => this.props.removeSigner(signer)}>
                 <RemoveIcon />
               </IconButton>
             </ListItemSecondaryAction>
           </ListItem>
         ))}
-        {editingNewSigner ? (
+        {editingSigner === "new" ? (
           <NewSignerForm
-            errors={newSignerErrors}
+            errors={signerEditErrors}
             onCancel={this.cancelEditingCosigner}
             onSubmit={this.createCosigner}
             onUpdate={this.updateNewSignerValues}
-            values={newSignerValues}
+            values={signerEditValues}
           />
         ) : null}
       </SpaciousList>
